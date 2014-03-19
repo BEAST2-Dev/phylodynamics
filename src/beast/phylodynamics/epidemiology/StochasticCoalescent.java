bcbe99da69a34d0bbb0b6e7f3e74d486b126ffda
tgvaughan@gmail.com <tgvaughan@gmail.com@8946df3d-76ec-2a93-8f20-38e0bdc2673e>
2014-03-19 23:44:31 +0000

    Marked StochasticCoalescent as a stochastic distribution.

--- src/beast/phylodynamics/epidemiology/StochasticCoalescent.java
+++ src/beast/phylodynamics/epidemiology/StochasticCoalescent.java
@@ -230,4 +230,9 @@ public class StochasticCoalescent extends TreeDistribution {
     protected boolean requiresRecalculation() {
         return ((CalculationNode) popSizeInput.get()).isDirtyCalculation() || super.requiresRecalculation();
     }
+
+    @Override
+    public boolean isStochastic() {
+        return popSizeInput.get().volzSIR.get() instanceof StochasticSIR;
+    }
 }
